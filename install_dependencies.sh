864ab93cecace86e20fbcd4ee0ac42792fd61b06
rcasal <38733103+rcasal@users.noreply.github.com>
2023-06-01 10:29:54 -0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@
 sudo apt install tesseract-ocr -y
 
 # Install Python packages
-pip install diffusers transformers scipy ftfy accelerate pytesseract -qq 
+pip install diffusers transformers scipy ftfy accelerate pytesseract hdbscan -qq 
 
 # Install Detectron2 from GitHub
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git' 
