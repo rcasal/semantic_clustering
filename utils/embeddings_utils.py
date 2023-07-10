c607c0cc9bf4975f8d2871d518e783f9496a6632
rcasal <38733103+rcasal@users.noreply.github.com>
2023-07-10 13:56:31 -0300

    Update embeddings_utils.py

--- utils/embeddings_utils.py
+++ utils/embeddings_utils.py
@@ -48,6 +48,22 @@ def add_bert_embeddings(df: pd.DataFrame, columns: str or list) -> pd.DataFrame:
     return df
 
 
+def get_clip_embeddings(text):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # Define the model ID
+    model_ID = "openai/clip-vit-base-patch32"
+    # Load pre-trained model and tokenizer
+    clip_model = CLIPModel.from_pretrained(model_ID)#.to(device)
+    clip_tokenizer = CLIPTokenizer.from_pretrained(model_ID)
+
+    # Tokenize input text and convert to PyTorch tensors
+    inputs = clip_tokenizer(text, return_tensors="pt")
+    text_embeddings = clip_model.get_text_features(**inputs)
+    # convert the embeddings to a numpy array
+    embedding_as_np = text_embeddings.cpu().detach().numpy()
+    return embedding_as_np
+
+
 def add_clip_embeddings(df: pd.DataFrame, columns: str or list) -> pd.DataFrame:
     """
     Add CLIP embeddings to the specified column(s) of the DataFrame.
@@ -67,28 +83,12 @@ def add_clip_embeddings(df: pd.DataFrame, columns: str or list) -> pd.DataFrame:
     else:
         raise ValueError("Invalid column parameter. Expected str or list.")
     
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    # Define the model ID
-    model_ID = "openai/clip-vit-base-patch32"
-    # Load pre-trained model and tokenizer
-    clip_model = CLIPModel.from_pretrained(model_ID)#.to(device)
-    clip_tokenizer = CLIPTokenizer.from_pretrained(model_ID)
-
-    def get_clip_embeddings(text):
-        # Tokenize input text and convert to PyTorch tensors
-        inputs = clip_tokenizer(text, return_tensors = "pt")
-        text_embeddings = clip_model.get_text_features(**inputs)
-        # convert the embeddings to numpy array
-        embedding_as_np = text_embeddings.cpu().detach().numpy()
-        return embedding_as_np
-
     for column in columns:
         print(f'Processing {column}...')
         if column != 'combined':
             df[f'clip_embedding_{column}'] = df[column].apply(get_clip_embeddings)
 
     return df
-
 
 def get_single_image_embedding(source_img_path):
     device = "cuda" if torch.cuda.is_available() else "cpu"
